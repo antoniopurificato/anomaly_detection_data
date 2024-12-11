8035cb67b439d8fce0fa8a16f3497ecca1f7b70f
antoniopurificato <purificato.2019135@studenti.uniroma1.it>
2024-12-11 22:49:54 +0100

    First commit.

--- test/boh.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import os
-
-df = pd.read_csv(os.getcwd() + '/../gold_test/gold_test.csv')
-n = len(df)
-df['Anomaly'] = np.random.choice([0, 1], size=n, p=[0.6, 0.4])
-df.to_csv(os.getcwd() + '/../gold_test/gold_test_2.csv', index=False)
